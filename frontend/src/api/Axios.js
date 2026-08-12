7458526509ed49f85ea60e682009a4141f1d7387
Krishna3558 <rajeevmishra6066@gmail.com>
2026-08-13 00:23:48 +0530

    fix the issue of login not working after expiring token

--- frontend/src/api/Axios.js
+++ frontend/src/api/Axios.js
@@ -14,4 +14,25 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
+API.interceptors.response.use(
+  (res) => res,
+
+  (error) => {
+    const status = error.response?.status;
+    const code = error.response?.data?.code;
+
+    if (
+      status === 401 &&
+      (code === "TOKEN_EXPIRED" || code === "TOKEN_INVALID")
+    ) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("userInfo");
+
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default API;
